7f5bbd852eff2136d113ec2f3fb55cfb14af6cda
Michal Kruszewski <kruszewski2@gmail.com>
2020-06-25 09:40:08 +0200

    Project created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WindowsPermissionChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
